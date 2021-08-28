241e589b1de2c206d16d8eb7760f68b642e984e1
Manish <manishjandu1@gmail.com>
2021-08-28 16:22:11 +0530

    instructions fragment setup

--- app/src/main/java/com/manishjandu/foodify/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/manishjandu/foodify/ui/fragments/instructions/InstructionsFragment.kt
@@ -2,9 +2,12 @@ package com.manishjandu.foodify.ui.fragments.instructions
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.manishjandu.foodify.R
 import com.manishjandu.foodify.databinding.FragmentsInstructionsBinding
+import com.manishjandu.foodify.models.Result
+import com.manishjandu.foodify.util.Constants.Companion.RECIPE_RESULT_KEY
 
 class InstructionsFragment : Fragment(R.layout.fragments_instructions) {
 
@@ -15,6 +18,12 @@ class InstructionsFragment : Fragment(R.layout.fragments_instructions) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentsInstructionsBinding.bind(view)
+
+        val args = arguments
+        val resultsBundle:Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        binding.webViewInstructions.webViewClient = object : WebViewClient(){}
+        binding.webViewInstructions.loadUrl(resultsBundle?.sourceUrl.toString())
     }
 
     override fun onDestroyView() {
