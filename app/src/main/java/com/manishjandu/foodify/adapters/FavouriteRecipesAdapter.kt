432c455f3f85c9d13798a49b6e64e9d5bff110d3
Manish <manishjandu1@gmail.com>
2021-08-29 16:18:04 +0530

    favourite recipe to detail activity setup

--- app/src/main/java/com/manishjandu/foodify/adapters/FavouriteRecipesAdapter.kt
+++ app/src/main/java/com/manishjandu/foodify/adapters/FavouriteRecipesAdapter.kt
@@ -2,26 +2,32 @@ package com.manishjandu.foodify.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.manishjandu.foodify.adapters.FavouriteRecipesAdapter.FavouriteRecipeViewHolder
 import com.manishjandu.foodify.data.database.entities.FavouriteEntity
 import com.manishjandu.foodify.databinding.RowLayoutFavouriteBinding
+import com.manishjandu.foodify.ui.fragments.favourites.FavouriteRecipesFragmentDirections
 
 
-class FavouriteRecipesAdapter:ListAdapter<FavouriteEntity, FavouriteRecipeViewHolder>(DiffUtilCallback()) {
+class FavouriteRecipesAdapter :
+    ListAdapter<FavouriteEntity, FavouriteRecipeViewHolder>(DiffUtilCallback()) {
 
-    class FavouriteRecipeViewHolder(private val binding:RowLayoutFavouriteBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(result: FavouriteEntity) {
-            binding.favouritesEntity = result
+    class FavouriteRecipeViewHolder(private val binding: RowLayoutFavouriteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+         val favouriteRecipeRowLayout = binding.favouriteRecipeRowLayout
+
+        fun bind(favouriteEntity: FavouriteEntity) {
+            binding.favouritesEntity = favouriteEntity
             binding.executePendingBindings()
         }
 
-        companion object{
+        companion object {
             fun from(parent: ViewGroup): FavouriteRecipeViewHolder {
                 val inflater = LayoutInflater.from(parent.context)
-                val binding = RowLayoutFavouriteBinding.inflate(inflater,parent,false)
+                val binding = RowLayoutFavouriteBinding.inflate(inflater, parent, false)
                 return FavouriteRecipeViewHolder(binding)
             }
         }
@@ -34,14 +40,27 @@ class FavouriteRecipesAdapter:ListAdapter<FavouriteEntity, FavouriteRecipeViewHo
     override fun onBindViewHolder(holder: FavouriteRecipeViewHolder, position: Int) {
         val currentRecipe = getItem(position)
         holder.bind(currentRecipe)
+        /**
+         * Single click listener
+         * */
+        holder.favouriteRecipeRowLayout.setOnClickListener {
+            val action =
+                FavouriteRecipesFragmentDirections.actionFavouriteRecipesFragmentToDetailsActivity(
+                    currentRecipe.result
+                )
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
-    class DiffUtilCallback(): DiffUtil.ItemCallback<FavouriteEntity>() {
+    class DiffUtilCallback() : DiffUtil.ItemCallback<FavouriteEntity>() {
         override fun areItemsTheSame(oldItem: FavouriteEntity, newItem: FavouriteEntity): Boolean {
             return oldItem === newItem
         }
 
-        override fun areContentsTheSame(oldItem: FavouriteEntity, newItem: FavouriteEntity): Boolean {
+        override fun areContentsTheSame(
+            oldItem: FavouriteEntity,
+            newItem: FavouriteEntity
+        ): Boolean {
             return oldItem.id == newItem.id
         }
     }
